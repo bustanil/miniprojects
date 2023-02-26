03d94c51fdb376e722b042ee8a625c7e9dd1f35f
Bustanil Arifin <bustanil@protonmail.com>
2023-02-26 10:31:40 +0800

    extract s3 bucket web config

--- journey/terraform/main.tf
+++ journey/terraform/main.tf
@@ -40,8 +40,17 @@ resource "aws_route53_record" "journey" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.website_bucket.website_domain
+    name                   =  aws_s3_bucket_website_configuration.website_config.website_domain
     zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
     evaluate_target_health = false
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "journey_web_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+// TODO bucket policy
