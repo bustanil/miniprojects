537c26d6ed44bd8dbb1db426dbeca97760f90881
Bustanil Arifin <bustanil@protonmail.com>
2023-02-26 10:14:42 +0800

    fix alias name

--- journey/terraform/main.tf
+++ journey/terraform/main.tf
@@ -40,8 +40,8 @@ resource "aws_route53_record" "journey" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.website_bucket.website_endpoint
+    name                   = aws_s3_bucket.website_bucket.website_domain
     zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
-    evaluate_target_health = true
+    evaluate_target_health = false
   }
 }
