dfcb787e9afe09ee2c45eea7ec2b6960fb8c7ea5
Bustanil Arifin <bustanil@protonmail.com>
2023-03-18 10:25:11 +0800

    generate HTML to ouput dir

--- journey/src/main.go
+++ journey/src/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	// "bytes"
+	"bytes"
 	"fmt"
 	"io/fs"
-	// "io/ioutil"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
   "regexp"
 
-	// "github.com/yuin/goldmark"
+	"github.com/yuin/goldmark"
 )
 
 var template string = `
@@ -34,15 +34,9 @@ func EndsWith(s string, endsWith string) bool {
 }
 
 func main() {
-	// var bs, err = ioutil.ReadFile("test.md")
-	// if err != nil {
-	// 	fmt.Println("error", err)
-	// 	return
-	// }
-
 	args := os.Args
 	input := args[1]
-	// output := args[2]
+	output := args[2]
 
 	rootFilePath, _ := filepath.Abs(input)
   re := regexp.MustCompile(`(?P<Year>\d{4})/(?P<Month>\d{2})/(?P<Day>\d{2})/(?P<Filename>.*)`)
@@ -50,20 +44,38 @@ func main() {
 		subDirPath := strings.ReplaceAll(path, rootFilePath, "")
 		if EndsWith(d.Name(), ".md") {
       matches := re.FindStringSubmatch(subDirPath)
-      yearIndex := re.SubexpIndex("Year")
-      fmt.Println(matches[yearIndex])
-      fmt.Println(matches[re.SubexpIndex("Filename")])
+      day := matches[re.SubexpIndex("Day")]
+      month := matches[re.SubexpIndex("Month")]
+      year := matches[re.SubexpIndex("Year")]
+      filename := matches[re.SubexpIndex("Filename")]
+      fmt.Println(year)
+      fmt.Println(filename)
+
+      var bs, err = ioutil.ReadFile(path)
+      if err != nil {
+        fmt.Println("error", err)
+        return err
+      }
+
+
+      fmt.Printf(`input: %s, output: %s`, input, output)
+      var buf bytes.Buffer
+      if err := goldmark.Convert(bs, &buf); err != nil {
+        panic(err)
+      }
+      out := fmt.Sprintf(template, string(buf.Bytes()))
+      outputDir := fmt.Sprintf("/%s/%s/%s/%s", output, year, month, day)
+      outputPath := fmt.Sprintf("/%s/%s", outputDir, filename)
+      os.MkdirAll(outputDir, 0777)
+      fmt.Println(outputPath)
+      err = ioutil.WriteFile(outputPath, []byte(out), 0766)
+      if err != nil {
+        fmt.Println("error", err)
+        return err
+      }
 		}
+
 		return nil
 	})
-
-	// fmt.Printf(`input: %s, output: %s`, input, output)
-	// var buf bytes.Buffer
-	// if err := goldmark.Convert(bs, &buf); err != nil {
-	// 	panic(err)
-	// }
-
-	// out := fmt.Sprintf(template, string(buf.Bytes()))
-	// ioutil.WriteFile("out.html", []byte(out), 0666)
 
 }
