c4caca0ec3ce3717934034270a6c9401de15ad29
Bustanil Arifin <bustanil@protonmail.com>
2023-03-31 23:32:23 +0800

    add comment, cleanup

--- journey/src/main.go
+++ journey/src/main.go
@@ -7,9 +7,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
-  "regexp"
-
+  "time"
 	"github.com/yuin/goldmark"
 )
 
@@ -24,8 +24,9 @@ var template string = `
 `
 
 type BlogEntry struct {
-	// date date
+  title string
 	path string
+  createdDate time.Time
 }
 
 func EndsWith(s string, endsWith string) bool {
@@ -38,8 +39,12 @@ func main() {
 	input := args[1]
 	output := args[2]
 
+
+  blogs := make([]BlogEntry, 0)
+
+  // walk through the input directory and find markdown files
 	rootFilePath, _ := filepath.Abs(input)
-  re := regexp.MustCompile(`(?P<Year>\d{4})/(?P<Month>\d{2})/(?P<Day>\d{2})/(?P<Filename>.*)`)
+  re := regexp.MustCompile(`(?P<Year>\d{4})/(?P<Month>\d{2})/(?P<Day>\d{2})/(?P<Filename>.*)\.md`)
 	filepath.WalkDir(input, func(path string, d fs.DirEntry, err error) error {
 		subDirPath := strings.ReplaceAll(path, rootFilePath, "")
 		if EndsWith(d.Name(), ".md") {
@@ -48,8 +53,18 @@ func main() {
       month := matches[re.SubexpIndex("Month")]
       year := matches[re.SubexpIndex("Year")]
       filename := matches[re.SubexpIndex("Filename")]
-      fmt.Println(year)
-      fmt.Println(filename)
+      title := strings.ReplaceAll(filename, "-", "")
+      createdDate, err := time.Parse("1-2-2006", fmt.Sprintf("%s-%s-%s", day, month, year))
+      if err != nil {
+        fmt.Println("error", err)
+        return err
+      }
+
+      blog := BlogEntry{
+        title: title,
+        path: subDirPath,
+        createdDate: createdDate,
+      }
 
       var bs, err = ioutil.ReadFile(path)
       if err != nil {
@@ -57,15 +72,17 @@ func main() {
         return err
       }
 
+      blogs = append(blogs, blog)
 
-      fmt.Printf(`input: %s, output: %s`, input, output)
+      // for each markdown file, generate the HTML page
+      fmt.Printf(`input: %s, output: %s\n`, input, output)
       var buf bytes.Buffer
       if err := goldmark.Convert(bs, &buf); err != nil {
         panic(err)
       }
       out := fmt.Sprintf(template, string(buf.Bytes()))
       outputDir := fmt.Sprintf("/%s/%s/%s/%s", output, year, month, day)
-      outputPath := fmt.Sprintf("/%s/%s", outputDir, filename)
+      outputPath := fmt.Sprintf("/%s/%s.html", outputDir, filename)
       os.MkdirAll(outputDir, 0777)
       fmt.Println(outputPath)
       err = ioutil.WriteFile(outputPath, []byte(out), 0766)
@@ -73,7 +90,9 @@ func main() {
         fmt.Println("error", err)
         return err
       }
+      fmt.Println(blogs)
 		}
+
 
 		return nil
 	})
